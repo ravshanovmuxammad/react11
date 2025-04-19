4aae6916deca86ec42a2b0c62a9675b97a833747
muxammadsodu <ravshanov.mmm777@gmail.com>
2025-04-19 10:53:08 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+function App() {
+
+  return (
+    <>
+     <h1>Ravshanov Muhammad Sodiq</h1>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis quisquam iste in assumenda animi at minima temporibus, deleniti quis enim.</p>
+<ul>
+  <li>HTML</li>
+  <li>CSS</li>
+  <li>Tailwind css</li>
+  <li>Java script</li>
+  <li>React</li>
+</ul>
+    </>
+  )
+}
+
+export default App
